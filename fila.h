3387440240ae4329c987568f148e7b09f5573c9a
Matheus <152931898+mateoclima@users.noreply.github.com>
2025-06-27 09:05:36 -0300

    Update fila.h
    Adicionado funções de fila e suas responsabilidades

---
+++ fila.h
@@ -0,0 +1,25 @@
+#ifndef FILA_H
+#define FILA_H
+
+#include "pacientes.h" 
+#include <stdbool.h>   
+
+// Função responsável por iniciar fila vazia:
+void iniciarFila(cabeca* fila);
+
+// Função que verifica se a fila está vazia:
+bool filaEstaVazia(cabeca* fila);
+
+// Função que insere um paciente mantendo a ordem de prioridade:
+void inserirPacienteNaFila(cabeca* fila, pacientes* novoPaciente);
+
+// Função que remove e retorna o paciente de maior prioridade:
+pacientes* atenderProximoPaciente(cabeca* fila);
+
+// Função que exibe todos os pacientes na fila:
+void visualizarFilaDeEspera(cabeca* fila);
+
+// Função que libera memória alocada pela fila:
+void liberarFila(cabeca* fila);
+
+#endif
